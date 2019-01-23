1e94e40f0d17fa79ea674f76202126b5f266c628
brian wilson <brianwilson@brians-MacBook-Pro-3.local>
2019-01-23 15:36:25 -0500

    add fixed_airline_index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 airlines1 = Airline.create(name: 'American Airlines', destinations:"Argentina", overall_rating: 5)
 
 airlines2 = Airline.create(name: 'Delta Airlines', destinations:'Morroccoa', overall_rating: 4)
+
+airlines3 = Airline.create(name: 'Delta Airlines', destinations:'Morroccoa', overall_rating: 4)
