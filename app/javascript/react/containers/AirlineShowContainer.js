ce77036d89d3502d3a465b7e258edfaa8caa3416
Brian Wilson <Brian.wilson1367@gmail.com>
2019-02-01 13:19:16 -0500

    fixing review submission

--- app/javascript/react/containers/AirlineShowContainer.js
+++ app/javascript/react/containers/AirlineShowContainer.js
@@ -13,6 +13,7 @@ class AirlineShowContainer extends Component {
       rating: "",
       reviews: []
     };
+    this.addNewReview = this.addNewReview.bind(this)
   }
   componentDidMount() {
     let id = this.props.params.id;
@@ -30,32 +31,62 @@ class AirlineShowContainer extends Component {
         });
       });
   }
+  handleSubmit(){
+    preventDefault();
+console.log("hello");
+  }
+
+  addNewReview(formPayload) {
+    let jsonStringInfo = JSON.stringify(formPayload);
+    fetch(`/api/v1/airlines/${this.props.id}/reviews`, {
+      method: "POST",
+      body: jsonStringInfo,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "same-origin"
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .then(responseBody=> responseBody.json())
+      .then(responseBody => {
+        debugger
+        this.setState({
+        reviews: this.state.reviews.concat(responseBody)
+      })
+      });
+  }
 
   render() {
     return (
-      <div className="flex-container">
-        <div class="airline_box">
+      <div>
           <AirlineShowTile
             key={this.props.params.id}
             airline={this.state.airline}
             destinations={this.state.destinations}
             rating={this.state.rating}
           />
-          <div class="airline_box">
-            <ReviewIndexContainer reviews={this.state.reviews} />
-          </div>
+          <ReviewIndexContainer reviews={this.state.reviews} />
           <div className="row">
             <div className="small-4 large-6 columns">
               <div className="panel callout radius">
                 <NewReviewFormContainer
                   key={this.props.params.id}
                   id={this.props.params.id}
+                  addNewReview={this.addNewReview}
                 />
               </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
